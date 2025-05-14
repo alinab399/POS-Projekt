ad3a530d94ccbf302990af9a61f28b3829a6f126
Startklar <Startklar@PC>
2025-05-14 14:15:32 +0200

    UserControlFenster für Trainingsgruppe erstellt. MainWindowXAML wurde ein DatePicker hinzugefügt sowie ein Button.

---
+++ PresenceLog_Sport/PresenceLog_Sport/TrainingsgruppeUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresenceLog_Sport
+{
+    /// <summary>
+    /// Interaktionslogik für TrainingsgruppeUserControl.xaml
+    /// </summary>
+    public partial class TrainingsgruppeUserControl : UserControl
+    {
+        public TrainingsgruppeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
