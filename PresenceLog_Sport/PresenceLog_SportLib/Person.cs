095c4d6ca90bf2693c1411ca84b6527782e109e5
Startklar <Startklar@PC>
2025-05-14 15:10:15 +0200

    Personen-Klasse aufgebaut.

--- PresenceLog_Sport/PresenceLog_SportLib/Person.cs
+++ PresenceLog_Sport/PresenceLog_SportLib/Person.cs
@@ -3,6 +3,26 @@ namespace PresenceLog_SportLib
 {
     public class Person
     {
+        private string Vorname { get; set; }
+        private string Nachname { get; set; }
+        private DateTime Geburtstag;
+        private List<bool> Answesend = new List<bool>();
+
+        public Person(string vorname, string nachname, DateTime geburtstag)
+        {
+
+        }
+
+
+        public void Answesenheit_Speichern(List<bool> answesend)
+        {
+
+        }
+
+        public void Answesenheit_Laden(List<bool> answesend)
+        {
+
+        }
     }
 
 }
