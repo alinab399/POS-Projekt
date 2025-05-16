c3945b2bdde702ed281190c0b69547136952ebda
Alina <Alina@LAPTOP-MNC9KJ7I>
2025-05-16 11:14:32 +0200

    Trainingsgruppe erstellen Button angefangen

--- PresenceLog_Sport/PresenceLog_Sport/TrainingsgruppeErstellen.xaml.cs
+++ PresenceLog_Sport/PresenceLog_Sport/TrainingsgruppeErstellen.xaml.cs
@@ -1,5 +1,8 @@
-﻿using PresenceLog_SportLib;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PresenceLog_Sport
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TrainingsgruppeErstellen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TrainingsgruppeErstellen : Window
     {
-        public MainWindow()
+        public TrainingsgruppeErstellen()
         {
             InitializeComponent();
-
-            
         }
-
-        
     }
 }
